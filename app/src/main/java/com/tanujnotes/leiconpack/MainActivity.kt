bc76a6f195f88ce60a78205ca06ab2f08cc5e74b
John Njuki <njukijohn2000@gmail.com>
2023-01-28 12:34:43 +0300

    update dependencies

--- app/src/main/java/com/tanujnotes/leiconpack/MainActivity.kt
+++ app/src/main/java/com/tanujnotes/leiconpack/MainActivity.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,7 +17,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LeIconPackTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                Surface(modifier = Modifier.fillMaxSize()) {
                     Greeting("Android")
                 }
             }
