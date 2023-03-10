cca914512aed314ce9f37b134e1c1bec6d61d7f9
kimsiele <sielekim.emmanuel@gmail.com>
2023-03-10 16:05:48 +0300

    Added app navigation

---
+++ app/src/main/java/com/tanujnotes/leiconpack/ui/NavGraph.kt
@@ -0,0 +1,29 @@
+package com.tanujnotes.leiconpack.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.tanujnotes.leiconpack.util.MenuItem
+
+@Composable
+fun NavGraph(
+    navController: NavHostController,
+    viewModel: MainViewModel,
+    showApplyDialog: MutableState<Boolean>,
+    selectedItem: MutableState<MenuItem>
+) {
+    NavHost(
+        navController = navController,
+        startDestination = MenuItem.Home.title.lowercase().trim()){
+
+        composable(route = MenuItem.Home.title.lowercase().trim()){
+            Dashboard(navController = navController, viewModel = viewModel, selectedItem, showApplyDialog = showApplyDialog)
+        }
+        composable(route = MenuItem.CustomIcons.title.lowercase().trim()){
+            WhyLeIconPack()
+        }
+    }
+
+}
