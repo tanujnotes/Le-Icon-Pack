1963ea8ac378f06e1a3182eb30be54aa8011df05
kimsiele <sielekim.emmanuel@gmail.com>
2023-06-15 12:35:20 +0300

    Show rating dialog

---
+++ app/src/main/java/com/tanujnotes/leiconpack/util/Extensions.kt
@@ -0,0 +1,46 @@
+package com.tanujnotes.leiconpack.util
+
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+
+object Extensions {
+    fun Context.openPlayStore() {
+        try {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id=$packageName")
+                ).addFlags(
+                    Intent.FLAG_ACTIVITY_NO_HISTORY or
+                            Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+                            Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+                )
+            )
+        } catch (e: ActivityNotFoundException) {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://play.google.com/store/apps/details?id=$packageName")
+                ).addFlags(
+                    Intent.FLAG_ACTIVITY_NO_HISTORY or
+                            Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+                            Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+                )
+            )
+        }
+    }
+
+    fun Context.openShareIntent() {
+        val shareIntent = Intent.createChooser(Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(
+                Intent.EXTRA_TEXT, "Check out this cool icon pack app. " +
+                        "https://play.google.com/store/apps/details?id=$packageName"
+            )
+            type = "text/plain"
+        }, "Choose App")
+        startActivity(shareIntent)
+    }
+}
